1151dc251cfdbbe459062432b8143e9104eb285c
SankalpMangure <samangure@gmail.com>
2022-10-18 14:53:07 +0530

    final change

--- question_five.js
+++ question_five.js
@@ -5,8 +5,11 @@ function long_palindrom_find(s) {
     let r = s.split('').reverse().join('');
     let p, l, m = [0, 0];
     for (p = 0; p < s.length; p++) {
-        for (l = s.length - p; l > m[1]; l--)
-            if (r.indexOf(s.substr(p, l)) > -1) m = [p, l];
+        for (l = s.length - p; l > m[1]; l--) {
+            if (r.indexOf(s.substr(p, l)) > -1) {
+                m = [p, l];
+            }
+        }
     }
     return s.substr(...m)
 }
--- question_three.js
+++ question_three.js
@@ -7,33 +7,34 @@ function areIsomorphic(str1, str2) {
     let m = str1.length;
     let n = str2.length;
 
-    if (m != n)
+    if (m != n) {
         return false;
+    }
 
     let marked = new Array(size);
-    for (let i = 0; i < size; i++)
+    for (let i = 0; i < size; i++) {
         marked[i] = false;
+    }
 
     let map = new Array(size);
     map.fill(0);
 
-    for (let i = 0; i < size; i++)
+    for (let i = 0; i < size; i++) {
         map[i] = -1;
+    }
 
     for (let i = 0; i < n; i++) {
-
         if (map[str1[i].charCodeAt()] == -1) {
 
-            if (marked[str2[i].charCodeAt()] == true)
+            if (marked[str2[i].charCodeAt()] == true) {
                 return false;
+            }
 
             marked[str2[i].charCodeAt()] = true;
-
             map[str1[i].charCodeAt()] = str2[i].charCodeAt();
-        }
-
-        else if (map[str1[i].charCodeAt()] != str2[i].charCodeAt())
+        } else if (map[str1[i].charCodeAt()] != str2[i].charCodeAt()) {
             return false;
+        }
     }
 
     return true;
